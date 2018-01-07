e775684249df28cb36dd9b81950c8fa514cc2b29
Miray Demirtas <35121495+mrydemirtas@users.noreply.github.com>
2018-01-07 15:58:25 +0300

    Konum alma ve değiştirme

---
+++ WindowsFormsApplication25/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication25
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int iks = 0, ye = 0;
+            iks = Cursor.Position.X;
+            ye = Cursor.Position.Y;
+            x.Text = iks.ToString();
+            y.Text = ye.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(int.Parse(x.Text.Trim()), int.Parse(y.Text.Trim()));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
